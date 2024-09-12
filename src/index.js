76a01ccc0ce23001abbec0a294b885df27363dbb
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-09-12 19:31:17 +0200

    primeras acciones

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
+import { increment } from "./redux/app-slice";
 import { store } from "./redux/store";
 
-console.log('hola');
-
 const state = store.getState();
-console.log(state);
-setMsg(state.msg)
+updateDom(state);
 
+store.subscribe(() => {
+  const state = store.getState();
+  updateDom(state);
+});
 
+document.getElementById('increment').addEventListener('click', function() {
+  store.dispatch(increment())
+});
 
+function updateDom(state) {
+  setMsg(state.msg);
+  setCounter(state.counter);
+}
 
 function setMsg(msg) {
   let msgElement = document.getElementById('msg');
   msgElement.innerText = msg;
+}
+function setCounter(counter) {
+  let counterElement = document.getElementById('counter');
+  counterElement.innerText = counter;
 }
--- src/redux/app-slice.js
+++ src/redux/app-slice.js
@@ -5,5 +5,15 @@ export const appSlice = createSlice({
   initialState: {
     msg: 'Mensaje inicial',
     counter: 0,
-  }
+  },
+  reducers: {
+    increment: (state) => {
+      state.counter++;
+    },
+    decrement: (state) => {
+      state.counter--;
+    },
+  },
 });
+
+export const { increment, decrement } = appSlice.actions;
