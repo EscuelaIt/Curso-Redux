9d9adc10df679ea0f290a47d57e549eb74d41ca5
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-09-19 19:28:01 +0200

    actions para un estado de carga

--- app-todo/src/index.js
+++ app-todo/src/index.js
@@ -3,6 +3,7 @@ import { store } from './redux/store';
 import { renderTodos } from './todo-render';
 import { renderFilters } from './filter-render';
 import './init';
+import { startLoading, stopLoading } from './redux/app-slice';
 
 store.subscribe( () => {
   const state = store.getState();
@@ -12,6 +13,7 @@ store.subscribe( () => {
   if(state.todo.todos.length == 0) {
     setMessage('No hay Todos!')
   }
+  setLoading(state.app.loading);
 });
 
 function setMessage(msg, status) {
@@ -23,3 +25,17 @@ function setMessage(msg, status) {
     messageElement.classList.remove('msg_error');
   }
 }
+
+function setLoading(loading) {
+  const loader = document.getElementById('loader');
+  if(loading) {
+    loader.classList.add('active');
+  } else {
+    loader.classList.remove('active');
+  }
+}
+
+document.getElementById('activarloading').addEventListener('click', function() {
+  store.dispatch(startLoading());
+  setTimeout(() => store.dispatch(stopLoading()), 5000);
+})
--- app-todo/src/redux/app-slice.js
+++ app-todo/src/redux/app-slice.js
@@ -4,18 +4,25 @@ export const appSlice = createSlice({
   name: 'app',
   initialState: {
     message: '',
-    statusMessage: ''
+    statusMessage: '',
+    loading: false,
   }, 
   reducers: {
     setMessage: (state, action) => {
       // action.payload = { msg: 'dfsdfd', status: 'error'}
       state.message = action.payload.msg;
       state.statusMessage = action.payload.status;
+    },
+    startLoading: (state) => {
+      state.loading = true;
+    },
+    stopLoading: (state) => {
+      state.loading = false;
     }
   }
 });
 
-export const { setMessage } = appSlice.actions;
+export const { setMessage, startLoading, stopLoading } = appSlice.actions;
 
 export const sendFeedback = (msg, status = 'success') => (dispatch) => {
   dispatch(setMessage({
